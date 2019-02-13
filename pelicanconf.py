c3d0fcb3cfab1edc21f75a9a511fd16e77bd1e80
Sarah H. McGrath <sarah@shmcgrath.com>
2019-02-12 20:27:43 -0500

    Update templates

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,14 +13,15 @@ THEME_STATIC_DIR = ''
 PATH = 'content'
 
 # static paths will be copied without parsing their contents
-STATIC_PATHS = ['extra', 'img']
-PAGE_EXCLUDES = ['extra', 'img']
-ARTICLE_EXCLUDES = ['extra', 'img']
+STATIC_PATHS = ['extra', 'img', 'robots.txt', 'docs']
+PAGE_EXCLUDES = ['extra', 'img', 'robots.txt', 'docs']
+ARTICLE_EXCLUDES = ['extra', 'img', 'robots.txt', 'docs']
 
 INDEX_SAVE_AS = 'blog/index.html'
 ARCHIVES_SAVE_AS = 'archives.html'
 TAGS_SAVE_AS = 'tags.html'
-AUTHORS_SAVE_AS = 'authors.html'
+AUTHORS_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -49,7 +50,7 @@ DEFAULT_CATEGORY = 'misc'
 DISPLAY_CATEGORIES_ON_MENU = False
 
 SLUGIFY_SOURCE = 'basename'
-ARTICLE_URL = 'blog/{slug}'
+ARTICLE_URL = 'blog/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{slug}.html'
 PAGE_URL = '{slug}'
 PAGE_SAVE_AS = '{slug}.html'
@@ -59,10 +60,22 @@ PAGE_SAVE_AS = '{slug}.html'
 # JINJA_FILTERS = {}
 
 # Plugins
-# Tipuse Search
+# Tipue Search
+# html_rst_directive This plugin allows you to use HTML tags from within reST documents.
+# .. html::
+#   (HTML code)
+# better_code_samples This plugin wraps all table blocks with a class attribute .codehilitetable in an additional div of class .hilitewrapper. It thus permits to style codeblocks better, especially to make them scrollable.
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['tipue_search']
-DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
+#PLUGINS = ['tipue_search', 'html_rst_directive', 'better_code_samples']
+DIRECT_TEMPLATES = ['index', 'tags', 'archives', 'search']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Custom Varioables for abby
+HAS_RESUME = True
+RESUME_PAGE = "Resume"
+RESUME_LOCATION = "/docs/SHMcGrathResume.pdf"
+
+ABOUT_SITE = True
